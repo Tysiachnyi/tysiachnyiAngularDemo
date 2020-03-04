4e3b1dc9e126093b3f647928add219d47bc1e55e
Nikita Tisyachniy <nikita.tisyachniy@lifecell.com.ua>
2020-03-04 13:05:00 +0200

    start working with template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    MainComponent,
+    AboutComponent,
+    ErrorComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
