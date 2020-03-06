d72209c1f0f9b40ef8f1ff0a3c4890f5e06dfd04
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-03-06 22:10:04 +0200

    added validators, create submit method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../data/posts.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-posts',
@@ -7,6 +8,7 @@ import { PostsService } from '../data/posts.service';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  form: FormGroup;
   addPostValue: boolean;
   inputValue: string;
   id: number;
@@ -16,22 +18,33 @@ export class PostsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup( {
+      title: new FormControl('', [
+        Validators.required,
+      ]),
+      text: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6)
+      ])
+    });
     this.addPostValue = false;
     this.inputValue = 'Show input';
     this.id = 100;
   }
 
   addInput() {
-    console.log(this.postsService.posts);
     this.addPostValue = !this.addPostValue;
     this.inputValue = this.addPostValue ? 'Hide input' : 'Show input';
   }
 
-  addPost() {
+    submit() {
     this.postsService.posts.push({
-      title: 'Hello',
-      text: 'yo',
-      id: this.id++
-    });
-  }
+        title: this.form.get('title').value,
+        text: this.form.get('text').value,
+        id: this.id++
+      });
+    this.form.reset();
+    this.addPostValue = !this.addPostValue;
+    this.inputValue = this.addPostValue ? 'Hide input' : 'Show input';
+    }
 }
