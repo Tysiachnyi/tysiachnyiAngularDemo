8e4bdbca950536143f6cbbb6fbb76ee82020a5c9
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-03-16 18:29:30 +0200

    created like and dislike method and view liked posts on user page

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -16,6 +16,7 @@ export class PostsComponent implements OnInit {
   modal: boolean;
   newPost: number;
   helper: Post[];
+  likedPost;
 
   constructor(
     public postsService: PostsService,
@@ -58,9 +59,28 @@ export class PostsComponent implements OnInit {
   }
 
   likePost(id: number) {
-    this.postsService.likedPosts = this.postsService.likedPosts
-        .concat(this.postsService.posts.find(d => d.id === id));
-    console.log(this.postsService.likedPosts);
+    // const found = this.postsService.likedPosts.some(el => el.id === id);
+    // if (!found) {
+    //   this.postsService.likedPosts = this.postsService.likedPosts
+    //       .concat(this.postsService.posts
+    //           .find(d => d.id === id));
+    //   this.likedPost = this.postsService.posts.find(el => el.id === id);
+    //   this.likedPost.liked = true;
+    //   console.log(this.postsService.likedPosts);
+    // }
+    this.postsService.like(id);
+  }
 
+  disLikePost(id: number) {
+    // const found = this.postsService.likedPosts.some(el => el.id === id);
+    // if (found) {
+    //   console.log(this.postsService.likedPosts);
+    //   this.postsService.likedPosts = this.postsService.likedPosts
+    //       .filter(d => d.id !== id);
+    //   console.log(this.postsService.likedPosts);
+    //   this.likedPost = this.postsService.posts.find(el => el.id === id);
+    //   this.likedPost.liked = false;
+    // }
+    this.postsService.dislike(id);
   }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -3,15 +3,17 @@ import {Injectable} from '@angular/core';
 export interface Post {
   title: string;
   text: string;
+  liked?: boolean;
   id: number;
 }
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  likedPost;
   posts: Post[] = [
     {title: 'New York times close', text: 'Sample text for post 1', id: 11},
     {title: 'Angular <3', text: 'Sample text for post 2', id: 22},
-    {title: 'Apple create 14,1 inch macbook pro', text: 'Sample text for post 3', id: 33},
+    {title: 'Apple create 14,1 inch MacBook pro', text: 'Sample text for post 3', id: 33},
     {title: 'Users need more privacy', text: 'Sample text for post 4', id: 44}
   ];
 
@@ -19,5 +21,29 @@ export class PostsService {
 
   getById(id: number) {
     return this.posts.find(p => p.id === id);
+  }
+  like(id: number) {
+    console.log(id);
+    const found = this.likedPosts.some(el => el.id === id);
+    if (!found) {
+      this.likedPosts = this.likedPosts
+           .concat(this.posts
+               .find(d => d.id === id));
+      this.likedPost = this.posts.find(el => el.id === id);
+      this.likedPost.liked = true;
+      console.log(this.likedPosts);
+     }
+  }
+  dislike(id: number) {
+    console.log(id);
+    const found = this.likedPosts.some(el => el.id === id);
+    if (found) {
+      console.log(this.likedPosts);
+      this.likedPosts = this.likedPosts
+          .filter(d => d.id !== id);
+      console.log(this.likedPosts);
+      this.likedPost = this.posts.find(el => el.id === id);
+      this.likedPost.liked = false;
+    }
   }
 }
